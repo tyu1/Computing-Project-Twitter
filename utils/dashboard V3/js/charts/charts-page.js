804ded9d939cc93dd184fac3d91e25e67bd5246e
tyu1 <0610yty@gmail.com>
2014-08-20 14:23:34 +1000

    add utils

---
+++ utils/dashboard V3/js/charts/charts-page.js
@@ -0,0 +1,126 @@
+// Flot chart 
+
+$(function () {
+    var d1 = [];
+    for (var i = 0; i < 14; i += 0.5)
+        d1.push([i, Math.sin(i)]);
+
+    var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+
+    // a null signifies separate line segments
+    var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+    
+    $.plot($("#flot"), [ d1, d2, d3 ]);
+});
+
+
+// ***************************** Pie chart ******************************** 
+$(function () {
+
+    // data
+    var data = [
+        { label: "Project 1",  data: 50},
+        { label: "Project 2",  data: 30},
+        { label: "Project 3",  data: 90},
+        { label: "Project 4",  data: 70},
+        { label: "Project 5",  data: 80},
+        { label: "Project 6",  data: 110}
+    ];
+       $.plot($("#pie"), data,
+{
+        series: {
+            pie: {
+                show: true,
+                radius: 1,
+                label: {
+                    show: true,
+                    radius: 2/3,
+                    formatter: function(label, series){
+                        return '<div style="font-size:8pt;text-align:center;padding:2px;color:white;">'+label+'<br/>'+Math.round(series.percent)+'%</div>';
+                    },
+                    threshold: 0.1
+                }
+            }
+        },
+        legend: {
+            show: false
+        }
+});
+    
+
+
+    // ***************************** DONUT chart ******************************** 
+    $.plot($("#donut"), data, 
+	{
+		series: {
+			pie: { 
+				innerRadius: 0.5,
+				show: true
+			}
+		}
+	});
+    });
+
+// ***************************** float chart 2 ******************************** 
+$(function () {
+    var sin = [], cos = [];
+    for (var i = 0; i < 14; i += 0.5) {
+        sin.push([i, Math.sin(i)]);
+        cos.push([i, Math.cos(i)]);
+    }
+
+    var plot = $.plot($("#float2"),
+           [ { data: sin, label: "sin(x)"}, { data: cos, label: "cos(x)" } ], {
+               series: {
+                   lines: { show: true },
+                   points: { show: true }
+               },
+               grid: { hoverable: true, clickable: true },
+               yaxis: { min: -1.2, max: 1.2 }
+             });
+
+    function showTooltip(x, y, contents) {
+        $('<div id="tooltip">' + contents + '</div>').css( {
+            position: 'absolute',
+            display: 'none',
+            top: y + 5,
+            left: x + 5,
+            border: '1px solid #fdd',
+            padding: '2px',
+            'background-color': '#fee',
+            opacity: 0.80
+        }).appendTo("body").fadeIn(200);
+    }
+
+    var previousPoint = null;
+    $("#float2").bind("plothover", function (event, pos, item) {
+        $("#x").text(pos.x.toFixed(2));
+        $("#y").text(pos.y.toFixed(2));
+
+        if ($("#enableTooltip:checked").length > 0) {
+            if (item) {
+                if (previousPoint != item.dataIndex) {
+                    previousPoint = item.dataIndex;
+                    
+                    $("#tooltip").remove();
+                    var x = item.datapoint[0].toFixed(2),
+                        y = item.datapoint[1].toFixed(2);
+                    
+                    showTooltip(item.pageX, item.pageY,
+                                item.series.label + " of " + x + " = " + y);
+                }
+            }
+            else {
+                $("#tooltip").remove();
+                previousPoint = null;            
+            }
+        }
+    });
+
+    $("#float2").bind("plotclick", function (event, pos, item) {
+        if (item) {
+            $("#clickdata").text("You clicked point " + item.dataIndex + " in " + item.series.label + ".");
+            plot.highlight(item.series, item.datapoint);
+        }
+    });
+});
---
+++ utils/dashboard V3/readme.txt
@@ -0,0 +1,6 @@
+Free icons from :http://www.smashingmagazine.com/2011/12/29/freebie-free-vector-web-icons-91-icons/
+   				http://webdesignledger.com/freebies/20-fresh-free-icon-sets
+
+
+
+
